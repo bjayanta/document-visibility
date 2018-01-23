c153cce6036da85bd0ba5636d7c4c83630830219
Jayanta Biswas <bjayanta.me@gmail.com>
2018-01-23 12:39:25 +0600

    create stylesheet and change the counter direction

--- js/app.js
+++ js/app.js
@@ -1,24 +1,43 @@
+// default variables
 var timer;
-var counter = 1;
+var counter = 20;
+var path    = "http://coderill.com";
 
-document.querySelector('#timer').addEventListener("click", function() {
+/**
+ * start timer
+ * decrease the counter variable
+ * and redirect to another url
+ *
+ */
+function startTimer() {
+	timer = setInterval(function() {
+		if(counter > 0) {
+			var counterString = (counter > 9) ? counter : ("0" + counter);
 
-    timer = setInterval(function() {
-        console.log("Hello World");
-        document.querySelector('h1').innerHTML = counter;
+			document.querySelector('#countdown').innerHTML = "00:" + counterString;
+			counter--;
+		} else {
+			window.location.href = path;
+		}
 
-        counter++;
-    }, 1000);
+	}, 1000);
+}
 
-});
+// start the timer
+startTimer();
 
+/**
+ * visibilitychange event monitor the document visible or not
+ * if not clear the timer interval (stop the timer)
+ * else start the timer after 1 seconds
+ *
+ */
 document.addEventListener("visibilitychange", function() {
-
-    if(document.hidden) {
-        clearInterval(timer);
-    }
-
-    console.log(document.hidden);
-    console.log(document.visibilityState);
-
+	if(document.visibilityState == "hidden") {
+		// stop interval
+		clearInterval(timer);
+	} else {
+		// restart after 1 seconds
+		setTimeout(startTimer, 1000);
+	}
 }, false);
